3ffe207523d202170f6f919a6d745ba27d522003
Roman Zipp <ich@ich.wtf>
2022-09-29 15:39:01 +0200

    Add captcha test

---
+++ tests/CaptchaTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace romanzipp\Turnstile\Tests;
+
+use romanzipp\Turnstile\Captcha;
+
+class CaptchaTest extends TestCase
+{
+    public function testChallenge()
+    {
+        self::assertSame('<div class="cf-turnstile"></div>', (string) Captcha::getChallenge());
+        self::assertSame('<div class="cf-turnstile" data-theme="dark"></div>', (string) Captcha::getChallenge('dark'));
+        self::assertSame('<div class="cf-turnstile" data-action="foo"></div>', (string) Captcha::getChallenge(null, 'foo'));
+        self::assertSame('<div class="cf-turnstile" data-cdata="foo"></div>', (string) Captcha::getChallenge(null, null, 'foo'));
+
+        self::assertSame('<div class="cf-turnstile" data-theme="dark" data-action="foo"></div>', (string) Captcha::getChallenge('dark', 'foo'));
+        self::assertSame('<div class="cf-turnstile" data-theme="dark" data-action="foo" data-cdata="bar"></div>', (string) Captcha::getChallenge('dark', 'foo', 'bar'));
+    }
+
+    public function testScript()
+    {
+        self::assertSame('<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>', (string) Captcha::getScript());
+        self::assertSame('<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>', (string) Captcha::getScript(true, true));
+        self::assertSame('<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" defer></script>', (string) Captcha::getScript(false, true));
+        self::assertSame('<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" ></script>', (string) Captcha::getScript(false, false));
+    }
+}
