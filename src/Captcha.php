b094a09e37588c679cfd82c229317e58a8d5978a
Michiel Vugteveen <info@it-can.nl>
2022-09-29 15:19:19 +0200

    add action to data attributes of div

--- src/Captcha.php
+++ src/Captcha.php
@@ -11,10 +11,24 @@ class Captcha
         return new HtmlString('<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>');
     }
 
-    public static function getChallenge(string $theme = 'auto'): HtmlString
+    public static function getChallenge(string $theme = 'auto', string $action = ''): HtmlString
     {
+        $parameters = collect([
+            'class'        => 'cf-turnstile',
+            'data-sitekey' => config('turnstile.site_key'),
+            'data-theme'   => $theme,
+            'data-action'  => $action,
+        ])
+            // Remove empty values
+            ->filter()
+            // Create key="value"
+            ->map(function ($item, $key) {
+                return "{$key}=\"{$item}\"";
+            })
+            ->implode(' ');
+
         return new HtmlString(
-            sprintf('<div class="cf-turnstile" data-sitekey="%s" data-theme="%s"></div>', config('turnstile.site_key'), $theme)
+            sprintf('<div %s></div>', $parameters)
         );
     }
 }
