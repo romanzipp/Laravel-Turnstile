11a89ce8c72f72805c2249c8d40bd0965dc3ce67
Roman Zipp <ich@ich.wtf>
2022-09-29 15:39:18 +0200

    Add more captcha method params

--- src/Captcha.php
+++ src/Captcha.php
@@ -6,29 +6,43 @@ use Illuminate\Support\HtmlString;
 
 class Captcha
 {
-    public static function getScript(): HtmlString
+    public static function getScript(bool $async = true, bool $defer = true): HtmlString
     {
-        return new HtmlString('<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>');
+        $attributes = [];
+
+        if ($async) {
+            $attributes[] = 'async';
+        }
+
+        if ($defer) {
+            $attributes[] = 'defer';
+        }
+
+        return new HtmlString(
+            sprintf('<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" %s></script>', implode(' ', $attributes))
+        );
     }
 
-    public static function getChallenge(string $theme = 'auto', string $action = ''): HtmlString
+    public static function getChallenge(?string $theme = null, ?string $action = null, ?string $cData = null): HtmlString
     {
-        $parameters = collect([
+        $parameters = [
             'class' => 'cf-turnstile',
             'data-sitekey' => config('turnstile.site_key'),
             'data-theme' => $theme,
             'data-action' => $action,
-        ])
-            // Remove empty values
-            ->filter()
-            // Create key="value"
-            ->map(function ($item, $key) {
-                return "{$key}=\"{$item}\"";
-            })
-            ->implode(' ');
+            'data-cdata' => $cData,
+        ];
+
+        $parameters = array_filter($parameters, fn ($value, string $key) => ! empty($value), ARRAY_FILTER_USE_BOTH);
 
         return new HtmlString(
-            sprintf('<div %s></div>', $parameters)
+            sprintf(
+                '<div %s></div>',
+                implode(
+                    ' ',
+                    array_map(fn (string $key, $value) => "{$key}=\"{$value}\"", array_keys($parameters), array_values($parameters))
+                )
+            )
         );
     }
 }
